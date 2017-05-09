cae3ea1228139dedac1be47f418ad9f9af1b9c31
Kanade <AbhijeetArvind.Kanade@standardbank.co.za>
2017-05-09 14:05:07 +0200

    Design completed

--- src/main/java/com/wipro/bank/assessment/App.java
+++ src/main/java/com/wipro/bank/assessment/App.java
@@ -16,55 +16,23 @@ public class App
 
 	public Account getAccountDetailsByID(int id)
 	{
-		Account emp1=null;
-		for(Account e:accounts)
-		{
-			if(e.getAccountID()==id){
-				emp1=e;
-			}
-
-		}
-		System.out.println(emp1);
-		return emp1;
-		
-
+		return null;
+	
 
 
 	}
 
 	public List<Account> getAccountDetailsByBalance(double amount)
 	{
-		emp2 =new ArrayList<Account>();          
-		for(Account e:accounts){                 
-			if(e.getBalance()>=amount){                     
-				emp2.add(e);
-			}
-		}
-		if(emp2.size()==0){
-			emp2=null;
-		}
-		System.out.println(emp2);
-		return emp2;
-
+		return null;
+		
 
 	}
 
 	public static void main( String[] args )
 	{
 
-		accounts.add(new Account(1,"acc1",101.11));
-		accounts.add(new Account(2,"acc2",201.13));
-		accounts.add(new Account(3,"acc3",301.22));
-		accounts.add(new Account(4,"acc4",401.55));
-		accounts.add(new Account(5,"acc5",501.77));
-		accounts.add(new Account(6,"acc6",601.22));
-		accounts.add(new Account(7,"acc7",701.11));
-		accounts.add(new Account(7,"acc8",801.99));
-
-		App temp = new App();
-		temp.getAccountDetailsByID(1);
-		temp.getAccountDetailsByBalance(501.77);
-
+	
 
 	}
 }
